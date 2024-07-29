4b29b529ba6a753e1075de78682b5f5f98adcc90
Milena Tyagra <milenatyagra.sc@gmail.com>
2024-07-29 18:01:51 -0300

    index de rotas

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+const userRoutes = require("./userRoutes")
+const ratingRoutes = require("./ratingRoutes")
+const authRoutes = require("./authRoutes")
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/users", userRoutes)
+app.use("/api/ratings", ratingRoutes)
+app.use("/api/authenticate", authRoutes)
+
+module.exports = app
